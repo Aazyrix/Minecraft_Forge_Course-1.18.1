e0cd15a6eae21a12f246b8bff7d865755eb2573d
Azyrix <aazyrix@gmail.com>
2022-04-18 08:16:40 -0500

    save 3

--- src/main/java/com/Azyrix/mccourse/block/ModBlocks.java
+++ src/main/java/com/Azyrix/mccourse/block/ModBlocks.java
@@ -19,8 +19,17 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKs =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MCCourseMod.MOD_ID);
 
+
+
     public static final RegistryObject<Block> COBALT_BLOCK = registerBlock("cobalt_block",
         () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+    public static final RegistryObject<Block> RAW_COBALT_BLOCK = registerBlock("raw_cobalt_block",
+        () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+    public static final RegistryObject<Block> DEEPSLATE_COBALT_ORE = registerBlock("deepslate_cobalt_ore",
+        () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
+
 
 
 
